4d8c564d7ef7166d40e727a330e84bc18fd8ff09
unknown <>
2024-11-10 13:47:31 -0800

    added, textured static; fixed models

--- scripts/stretch-table.gd
+++ scripts/stretch-table.gd
@@ -4,14 +4,13 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("start")
-	stretch("z")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print($Tabletop.global_position)
-	print($Tabletop/StaticPin.global_position)
-	print(stretcher.is_playing())
-	print("-------")
+	#print($Tabletop.global_position)
+	#print($Tabletop/StaticPin.global_position)
+	#print(stretcher.is_playing())
+	#print("-------")
 	pass
 
 func stretch(axis) -> void:
